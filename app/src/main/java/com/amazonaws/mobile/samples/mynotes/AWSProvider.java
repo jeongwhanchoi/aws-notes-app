e412a88b4c755fa8515409f246e824f08d63fbbc
jeongwhanchoi <jeongwhan.choi@icloud.com>
2018-04-09 11:34:18 +0900

    Add Data access methods to the AWSProvider class

--- app/src/main/java/com/amazonaws/mobile/samples/mynotes/AWSProvider.java
+++ app/src/main/java/com/amazonaws/mobile/samples/mynotes/AWSProvider.java
@@ -13,12 +13,20 @@ import com.amazonaws.mobile.auth.userpools.CognitoUserPoolsSignInProvider;
 import com.amazonaws.mobile.config.AWSConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointManager;
+// Add DynamoDBMapper and AmazonDynamoDBClient to support data access methods
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 public class AWSProvider {
     private static AWSProvider instance = null;
     private Context context;
     private AWSConfiguration awsConfiguration;
     private PinpointManager pinpointManager;
+
+    // Declare DynamoDBMapper and AmazonDynamoDBClient private variables
+    // to support data access methods
+    private AmazonDynamoDBClient dbClient = null;
+    private DynamoDBMapper dbMapper = null;
 
     public static AWSProvider getInstance() {
         return instance;
@@ -59,5 +67,17 @@ public class AWSProvider {
             pinpointManager = new PinpointManager(config);
         }
         return pinpointManager;
+    }
+
+    public DynamoDBMapper getDynamoDBMapper() {
+        if (dbMapper == null) {
+            final AWSCredentialsProvider cp = getIdentityManager().getCredentialsProvider();
+            dbClient = new AmazonDynamoDBClient(cp);
+            dbMapper = DynamoDBMapper.builder()
+                    .awsConfiguration(getConfiguration())
+                    .dynamoDBClient(dbClient)
+                    .build();
+        }
+        return dbMapper;
     }
 }
